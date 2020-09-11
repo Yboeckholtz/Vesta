f2407319937564b098b8c0cae2667391d110d41e
Youri <yboeckholtz@gmail.com>
2020-09-11 15:42:25 +0200

    keyboard view retrieving data from json file now and toggle button fixed

--- kassa/assets/js/myscripts.js
+++ kassa/assets/js/myscripts.js
@@ -1,4 +1,15 @@
 function toggle() {
     $('.keyboardViewSection').toggle();
     $('.touchViewSection').toggle();
+
+        var x = document.getElementById("toggleBtn");
+        if (x.innerHTML === "Keyboard View") {
+            x.innerHTML = "Touch View";
+        } else {
+            x.innerHTML = "Keyboard View";
+        }
+
 }
+
+
+
