53943c93f05240f143ef70f1aa000eeb5631829f
Youri <yboeckholtz@gmail.com>
2020-09-16 14:54:38 +0200

    datatable working, needs some styling

--- kassa/assets/js/myscripts.js
+++ kassa/assets/js/myscripts.js
@@ -11,56 +11,41 @@ function toggle() {
         }
 }
 //retrieve products from json file
-    $(document).ready(function(){
+    $(document).ready(function() {
 
         //retrieve touchView products
-            $.getJSON("assets/products/sample_products.json", function(data){
-                var product_data = '';
-                $.each(data, function (key, value){
-                    product_data += '<div class="col-3 productCard">';
-                    product_data += '<a href="#" class="productItem">';
-                    product_data += '<div class="card">';
-                    product_data += '<img src="assets/images/Firecracker.jpg" alt="Avatar" style="width:100%; height: 8vh;">';
-                    product_data += '<div class="container">';
-                    product_data += '<p>'+ value.name + '</p>';
-                    product_data += '</div>';
-                    product_data += '</div>';
-                    product_data += '</a>';
-                    product_data += '</div>';
-                });
-                $('#touchViewProducts').append(product_data);
-
 //retrieve keyboardView products
-            $.getJSON("assets/products/sample_products.json", function(data){
-                var product_data = '';
-                $.each(data, function (key, value){
-                    product_data += '<tr>';
-                    product_data += '<td>'+value.article_number+'</td>';
-                    product_data += '<td>'+value.name+'</td>';
-                    product_data += '<td>'+value.stock+'</td>';
-                    product_data += '<td>'+'&euro;'+ value.price+'</td>';
-                    product_data += '<td id="keyboardViewButtonTr">'
-                        +"<button type=\"button\" class=\"btn addFree\">Add Free</button>"
-                        +"<button type=\"button\" class=\"btn addToCart\">Add to Cart</button>"
-                        +'</td>';
-                    product_data += '</tr>';
-                });
-                $('#productTable').append(product_data);
-            });
-
+        $('#data-table').DataTable({
+            "paging": false,
+            "ajax": "assets/products/sample_products.json",
+            "columns": [
+                {"data": "article_number"},
+                {"data": "name"},
+                {"data": "stock"},
+                {"data": "price"},
+                {"defaultContent": "<button class='addToCart'>Add to Cart</button>"},
+            ]
         });
 
-        //   <div class="col-3 productCard">
-//         <a href="#" class="productItem" data-name="Celebration cracker 90 schots" data-price="50000" data-id="1">
-//             <div class="card">
-//                 <img src="assets/images/Firecracker.jpg" alt="Avatar" style="width:100%; height: 9vh;">
-//                     <div class="container">
-//                         <p> Rotje</p>
-//                     </div>
-//             </div>
-//         </a>
-//     </div>
 
+        $.getJSON("assets/products/sample_products.json", function(data){
+            var product_data = '';
+            $.each(data, function (key, value){
+                product_data += '<div class="col-3 productCard">';
+                product_data += '<a href="#" class="productItem">';
+                product_data += '<div class="card">';
+                product_data += '<img src="assets/images/Firecracker.jpg" alt="Avatar" style="width:100%; height: 8vh;">';
+                product_data += '<div class="container">';
+                product_data += '<p>'+ value.name + '</p>';
+                product_data += '</div>';
+                product_data += '</div>';
+                product_data += '</a>';
+                product_data += '</div>';
+            });
+            $('#touchViewProducts').append(product_data);
+
+        });
+    })
     //Search function
 //     function searchTable() {
 //     var input, filter, found, table, tr, td, i, j;
@@ -83,6 +68,7 @@ function toggle() {
 // }
 // }
 // }
-});
+
+
 
 
