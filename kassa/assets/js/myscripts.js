17828b9cba395a37a00c0126f3a5c59408e6d8dd
Youri <yboeckholtz@gmail.com>
2020-09-16 16:33:15 +0200

    touch and keyboard retreiving data

--- kassa/assets/js/myscripts.js
+++ kassa/assets/js/myscripts.js
@@ -23,28 +23,10 @@ function toggle() {
                 {"data": "name"},
                 {"data": "stock"},
                 {"data": "price"},
-                {"defaultContent": "<button class='addToCart'>Add to Cart</button>"},
+                {"defaultContent": "<button class='addToCart'>Add to Cart</button>"}
             ]
         });
 
-
-        $.getJSON("assets/products/sample_products.json", function(data){
-            var product_data = '';
-            $.each(data, function (key, value){
-                product_data += '<div class="col-3 productCard">';
-                product_data += '<a href="#" class="productItem">';
-                product_data += '<div class="card">';
-                product_data += '<img src="assets/images/Firecracker.jpg" alt="Avatar" style="width:100%; height: 8vh;">';
-                product_data += '<div class="container">';
-                product_data += '<p>'+ value.name + '</p>';
-                product_data += '</div>';
-                product_data += '</div>';
-                product_data += '</a>';
-                product_data += '</div>';
-            });
-            $('#touchViewProducts').append(product_data);
-
-        });
     })
     //Search function
 //     function searchTable() {
