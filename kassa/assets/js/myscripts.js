8830cec9a803668f566ba19622d23b931b28a110
Youri <yboeckholtz@gmail.com>
2020-09-25 15:52:05 +0200

    updated some functions

--- kassa/assets/js/myscripts.js
+++ kassa/assets/js/myscripts.js
@@ -11,67 +11,33 @@ function toggle() {
     }
 }
 //
+
 var shopcart = [];
 
-$(document).ready(function() {
-//         //add to cart functionality (onclick)
-    outputCart();
-    $(document).on('click', '.productItem', function(){
-        var productInfo = $(this.dataset)[0];
-        productInfo.qty = 1;
-        var productInCart = false;
+//remove from cart function
+var removeCartItemButtons = document.getElementsByClassName('close')
+console.log(removeCartItemButtons)
+for (var i = 0; i < removeCartItemButtons.length; i ++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+    })
+}
 
-        $.each(shopcart, function(index, value){
-            if(value.id == productInfo.id){
-                value.qty=parseInt(value.qty) + parseInt(productInfo.qty);
-                productInCart = true;
-            }
-        })
+function updateCartTotal() {
+    var carItemContainer = document.getElementsByClassName('shopcardProducts')[0]
+    car
+}
 
-        if(!productInCart){
-            shopcart.push(productInfo);
-        }
-        sessionStorage["sc"]=JSON.stringify(shopcart);
-        outputCart();
-    });
 
-//output cart
-    function outputCart(){
-        if(sessionStorage["sc"] != null){
-            shopcart = JSON.parse(sessionStorage["sc"].toString());
-            console.log(sessionStorage["sc"]);
-        }
-        var holderHTML = "";
-        var total = 0;
+//
+// const TouchView = {
+//     filterProducts: function(){
+//
+//     }
+// }
 
-        $.each(shopcart, function(index, value){
-            console.log(value);
-            var subtotal = value.qty * value.price;
-            total = (value.price * value.qty);
-            holderHTML += '<div class="col-5">' + value.name + '</div>' +
-                '<div class="col-1">' + value.qty + '</div>' +
-                '<div class="col-2">' + '&euro;' + value.price + '</div>' +
-                '<div class="col-3">' + '&euro;' + subtotal.toFixed(2) + '</div>'
-        })
-        // holderHTML += '<div>'+ total +'</div>';
-        $('#output').html(holderHTML);
-    }
-
-    //categorie filter
-    $('[data-selected]')
-    $('#data-table').DataTable({
-        "paging": false,
-        "ajax": "assets/products/sample_products.json",
-        "columns": [
-            {"data": "article_number"},
-            {"data": "name"},
-            {"data": "stock"},
-            {"data": "price", render: $.fn.dataTable.render.number( ',', '.', 2, '&euro; ' )},
-            {"defaultContent": "<button class='addToCart'>Add to Cart</button><button class='addFree'>Add Free</button>"}
-        ]
-    });
-
-})
 
 const Cart = {
     items: [''],
@@ -96,12 +62,13 @@ const Cart = {
 }
 
 const Product = class{
-    constructor(id, article_number, name, stock, price) {
+    constructor(id, article_number, name, stock, price, categorie) {
         this.id = id;
         this.article_number = article_number;
         this.name = name;
         this.stock = stock;
         this.price = price;
+        this.categorie = categorie;
     }
 };
 
@@ -117,35 +84,20 @@ const Assortment = class {
             self.products.push(pr);
         });
     }
-
-    filterProducts(products){}
 }
+
 
 Cart.reset();
 Cart.add('apenkots');
 console.log(Cart.items);
 
-//filter function
-// $(".nav-link").click(function() {
-//     var category = $(this).attr('id');
-//     if (category != "all") {
-//         $(".productCard").hide();
-//         $(".productCard").each(function() {
-//             if ($(this).find(".productItem").attr('categories') == category) {
-//                 $(this).show() //show that div
-//             }
-//         })
-//     } else {
-//         $(".productCard").show();
-//     }
-// })
 
 //load in items to touchview
 $(function(){
     $.getJSON("assets/products/sample_products.json", function(response) {
         $.each(response.data, function (i, el) {
             let card = $($('#productCard-template').html());
-            card.find('#cardName').html( el.name );
+            card.find('#cardName').html( el.name);
             card.find('#cardPrice').html( '&euro;' + el.price );
             card.find('.productItem').attr('data-price', el.price)
                 .attr('data-article-number', el.article_number)
@@ -158,6 +110,75 @@ $(function(){
     });
 });
 
+//onlick function that adds data to template in cart
+$(function(){
+    $.getJSON("assets/products/sample_products.json", function(response) {
+        $.each(response.data, function (i, el) {
+
+        })
+    })
+})
+
+$(document).ready(function() {
+//         //add to cart functionality (onclick)
+    outputCart();
+    $(document).on('click', '.productItem', function(){
+        var productInfo = $(this.dataset)[0];
+        productInfo.qty = 1;
+        var productInCart = false;
+
+        $.each(shopcart, function(index, value){
+            if(value.id == productInfo.id){
+                value.qty=parseInt(value.qty) + parseInt(productInfo.qty);
+                productInCart = true;
+            }
+        })
+
+        if(!productInCart){
+            shopcart.push(productInfo);
+        }
+        sessionStorage["sc"]=JSON.stringify(shopcart);
+        outputCart();
+    });
+
+
+//output cart
+    function outputCart(){
+        if(sessionStorage["sc"] != null){
+            shopcart = JSON.parse(sessionStorage["sc"].toString());
+            // console.log(sessionStorage["sc"]);
+        }
+        var holderHTML = "";
+        var total = 0;
+
+        $.each(shopcart, function(index, value){
+            // console.log(value);
+            var subtotal = value.qty * value.price;
+            total = (value.price * value.qty);
+            holderHTML += '<div class="col-5">' + value.name + '</div>' +
+                '<div class="col-1">' + value.qty + '</div>' +
+                '<div class="col-2">' + '&euro;' + value.price + '</div>' +
+                '<div class="col-3">' + '&euro;' + subtotal.toFixed(2) + '</div>' +
+                '<button type="button" class="close"> <span aria-hidden="true">&times;</span> </button>'
+        })
+        // holderHTML += '<div>'+ total +'</div>';
+        $('#output').html(holderHTML);
+    }
+
+    //datatable
+    $('[data-selected]')
+    $('#data-table').DataTable({
+        "paging": false,
+        "ajax": "assets/products/sample_products.json",
+        "columns": [
+            {"data": "article_number"},
+            {"data": "name"},
+            {"data": "stock"},
+            {"data": "price", render: $.fn.dataTable.render.number( ',', '.', 2, '&euro; ')},
+            {"defaultContent": "<button class='addToCart'>Add to Cart</button><button class='addFree'>Add Free</button>"}
+        ]
+    });
+})
 
 
 
